8c1a4c19e744d14b970bb4a58645a27e5e41d563
Kirill Lushin <you@example.com>
2023-09-10 14:16:05 +0300

    Очередной коммит из дома с основной машины

---
+++ 20230910bchaintrain.py
@@ -0,0 +1,18 @@
+import urllib.request
+import json
+
+count = 0
+miners = []
+for i in range(0, 20):
+    url = f"https://blockchain.info/block-height/{45670}?format=json"
+    r = urllib.request.urlopen(url)
+    data = json.load(r)
+    sp = data['blocks'][0]['tx'][0]['out'][0]['spent']
+    addr = data['blocks'][0]['tx'][0]['out'][0]['addr']
+    if sp == False:
+        count += 1
+        miners.append(addr)
+print (count)
+print(miners)
+
+
