f65c1d014ff36e06da141faaef970bd66a186f76
Kirill Lushin <you@example.com>
2023-09-10 14:23:08 +0300

    Очередной коммит из дома с основной машины

--- 20230910bchaintrain.py
+++ 20230910bchaintrain.py
@@ -4,7 +4,7 @@ import json
 count = 0
 miners = []
 for i in range(0, 20):
-    url = f"https://blockchain.info/block-height/{45670}?format=json"
+    url = f"https://blockchain.info/block-height/{34}?format=json"
     r = urllib.request.urlopen(url)
     data = json.load(r)
     sp = data['blocks'][0]['tx'][0]['out'][0]['spent']
