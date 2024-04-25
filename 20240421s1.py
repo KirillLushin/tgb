77beb692629321b3c8124be22e8e8b20c03ab835
Kirill Lushin <you@example.com>
2024-04-25 22:46:41 +0300

    Очередной коммит из дома с основной машины

--- 20240421s1.py
+++ 20240421s1.py
@@ -1,4 +1,9 @@
+import time
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.os_manager import ChromeType
+from selenium.webdriver.chrome.service import Service as ChromiumService
 
-driver = webdriver.Chrome()
-driver.get("http://example.com")
+with webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install())) as driver:
+    driver.get("https://domik-mo.ru/upravlyayushchaya-kompaniya/7595849")
+    time.sleep(15)
